3e633dba92acb92302ad254a5aafb1b326cbc518
Anastasya <okayota83@mail.ru>
2022-10-01 22:37:20 +0400

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿Console.WriteLine("Введите число от 1 до 7");
+int x = int.Parse(Console.ReadLine());
+int a = 1;
+int b = 2;
+int c = 3;
+int d = 4;
+int e = 5;
+int f = 6;
+int g = 7;
+
+
+if (x==a)
+{
+    Console.WriteLine("Monday");
+} 
+
+if (x==b)
+{
+    Console.WriteLine("Tuesday");
+} 
+
+if (x==c)
+{
+    Console.WriteLine("Wednesday");
+} 
+
+if (x==d)
+{
+    Console.WriteLine("Thersday");
+} 
+
+if (x==e)
+{
+    Console.WriteLine("Friday");
+} 
+
+if (x==f)
+{
+    Console.WriteLine("Saterday");
+} 
+
+if (x==g)
+{
+    Console.WriteLine("Sunday");
+} 
+
+
+
+    
