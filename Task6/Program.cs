31a5bc8b809222686daf6ae96adf1363c09354df
Anastasya <okayota83@mail.ru>
2022-10-02 12:12:47 +0400

    add Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите N");
+int N = int.Parse(Console.ReadLine());
+int N1 = 1;
+
+
+while (N1<=N)
+    {
+        int x = N1++;
+        if (x%2==0)
+        Console.Write(x);
+    }
+     
+
+
