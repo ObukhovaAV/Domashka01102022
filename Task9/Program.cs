a581ce1e6985cf1dcd7adbf329bc0845420a584f
Anastasya <okayota83@mail.ru>
2022-10-03 15:47:07 +0400

    add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число от 10 до 99");
+int n = int.Parse(Console.ReadLine());
+int first = n/10;
+int second = n%10;
+if (first>second)
+{
+    Console.Write(first);
+}
+else if (first<second)
+{
+    Console.Write(second);
+}
+else 
+{
+    Console.Write("Цифры равны");
+}
