8965708eee0138459f5baf6ef36ff5c3c02104ea
Anastasya <okayota83@mail.ru>
2022-10-04 21:19:48 +0400

    add Task12, Task12.1

---
+++ Task12.1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введите число");
+long number = long.Parse(Console.ReadLine() ?? "0");
+
+
+if (number/100>0)
+{
+   while (number/1000>0)
+   {
+    number = number/10;
+   }
+   long third = number%10;
+
+Console.Write(third);
+}
+else
+{
+    Console.Write("В этом числе нет третьей цифры");
+}
+   
---
+++ Task12/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число");
+long number = long.Parse(Console.ReadLine() ?? "0");
+int a = 99;
+int b = 999;
+
+if (number>a)
+{
+   while (number>b)
+   {
+    number = number/10;
+   }
+   long third = number%10;
+
+Console.Write(third);
+}
+else
+{
+    Console.Write("no");
+}
+   
+
